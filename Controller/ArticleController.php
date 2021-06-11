51b015b65f921aefaf1aaed846d7d3d532b01131
David Bouscarle <d.bouscarle@gmail.com>
2021-06-11 11:46:34 +0200

    Change and add local variables in constructor method in Article Controller

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -4,20 +4,17 @@
 class ArticleController
 {
     private string $pdo;
-    private string $dbName = 'news_website';
-    private string $dbHost = 'localhost';
-    private string $dbUser = 'root';
-    private string $dbPass = '';
 
     /**
-     * ArticleController constructor.
-     * @param string $dbName
-     * @param string $dbHost
-     * @param string $dbUser
-     * @param string $dbPass
+     * ArticleController constructor
      */
-    public function __construct(string $dbName, string $dbHost, string $dbUser, string $dbPass)
+    public function __construct()
     {
+        $dbName = 'news_website';
+        $dbHost = 'localhost';
+        $dbUser = 'root';
+        $dbPass = '';
+
         $this->setPdo(new PDO("mysql:host=$dbHost;dbname=$dbName, $dbUser, $dbPass"));
     }
 
