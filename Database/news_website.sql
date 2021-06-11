a64b8246300488790204108f9d0158d3c7f677b5
David Bouscarle <d.bouscarle@gmail.com>
2021-06-11 19:13:03 +0200

    Add ENGINE to Database

--- Database/news_website.sql
+++ Database/news_website.sql
@@ -5,7 +5,7 @@ CREATE TABLE article (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content TEXT(20000) NOT NULL
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE comment (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -14,7 +14,7 @@ CREATE TABLE comment (
     FOREIGN KEY (article_id)
         REFERENCES article(id)
         ON DELETE CASCADE
-);
+)ENGINE=InnoDB;
 
 INSERT INTO article (id, title, content)
 VALUES (1, 'Comment faire du pain avec ses pieds ?', 'Avec les mains, mais avec tendresse');
