fe9fc9f0aa5740aa1b7e95e1a54ab6bf8d24aa7d
David Bouscarle <d.bouscarle@gmail.com>
2021-06-11 09:39:49 +0200

    Configure Database Console and Modify some details on SQL COMMAND

--- Database/news_website.sql
+++ Database/news_website.sql
@@ -4,12 +4,12 @@ CREATE DATABASE news_website CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE article (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL
+    content TEXT(20000) NOT NULL
 );
 
 CREATE TABLE comment (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    content TEXT NOT NULL,
+    content TEXT(20000) NOT NULL,
     article_id INT(10) NOT NULL,
     FOREIGN KEY (article_id)
         REFERENCES article(id)
