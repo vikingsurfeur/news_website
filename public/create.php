137d077310dc80fafd0a42ee7fcedd469541d362
David Bouscarle <d.bouscarle@gmail.com>
2021-06-12 19:00:19 +0200

    Work on create.php

--- public/create.php
+++ public/create.php
@@ -4,20 +4,26 @@ require_once './templates/header.php';
 
 $controller = new ArticleController();
 
+if ($_POST)
+{
+    $article = new Article($_POST);
+    $controller->create($article);
+}
+
 ?>
 
 <div class="container">
     <h3 class="m-5">Création de l'article</h3>
     <form method="POST">
         <div class="form-group m-5">
-            <label for="title">Modification du titre</label>
-            <input class="form-control" type="text" id="title" name="title" value="<?= $article->setTitle() ?>">
+            <label for="title">Titre de l'article</label>
+            <input class="form-control" type="text" id="title" name="title" value="">
         </div>
         <div class="form-group m-5">
-            <label for="content">Modification du contenu</label>
-            <textarea class="form-control" type="text" id="content" name="content" cols="20" rows="5" ><?= $article->setContent() ?></textarea>
+            <label for="content">Contenu de l'article</label>
+            <textarea class="form-control" type="text" id="content" name="content" cols="20" rows="5" ></textarea>
         </div>
-        <button type="submit" class="btn btn-warning m-5">Publier l'article'</button>
+        <button type="submit" class="btn btn-success m-5">Publier l'article</button>
     </form>
 </div>
 
