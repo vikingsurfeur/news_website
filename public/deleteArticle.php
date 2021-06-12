3d1fb4fb9618549c09c95faabad17fcfa80969ac
David Bouscarle <d.bouscarle@gmail.com>
2021-06-12 08:43:42 +0200

    Implement the delete method in deleteArticle.php and give a redirection to index.php after the delete command, it's works...

--- public/deleteArticle.php
+++ public/deleteArticle.php
@@ -1,34 +1,10 @@
 <?php
 
-include './templates/header.php';
+require_once './templates/header.php';
 
 $controller = new ArticleController();
+$controller->delete($_GET['id']);
 
 ?>
 
-<div class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Vous êtes sur le point de supprimer un article</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Etes-vous bien sûr de vouloir supprimer l'article n° <?= $_GET['id'] ?></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-success">J'en suis sûr !</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Je ne sais pas en en fait...</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<?php
-
-require_once './templates/footer.php';
-
-?>
+<script>window.location.href="index.php"</script>
