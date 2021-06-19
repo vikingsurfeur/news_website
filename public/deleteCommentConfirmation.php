039bb23bcc424146c35e55ea3f09d57804380b4c
David Bouscarle <d.bouscarle@gmail.com>
2021-06-19 12:39:00 +0200

    Add a deleteCommentConfirmation.php to confirm the comment delete. Add the same method for article delete. Refractoring and format some code in PHP files.

--- public/deleteCommentConfirmation.php
+++ public/deleteCommentConfirmation.php
@@ -4,9 +4,11 @@
     $controller = new CommentController();
     $comment = $controller->read($_GET["id"]);
     $articleId = $comment->getArticle_id();
+    $commentContent = substr($comment->getContent(), 0,40);
 
     echo "<script>
-            const result = window.confirm(`Are you sure to delete this comment ?`);
+            const result = window.confirm
+                (`Are you sure to delete this comment ?\n{$commentContent}...`);
             
             result ? 
             window.location.href='./deleteComment.php?id={$_GET["id"]}' :
